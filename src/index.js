c48aa80c56b860c1193dc979fce928b371ae9ba2
Cody Goss <c.ward.goss@gmail.com>
2019-03-06 10:54:04 -0500

    adds new project

--- src/index.js
+++ src/index.js
@@ -1,73 +1,85 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 
-const Button = props => (
-  <button className={props.buttonStyle} onClick={props.handler}>
-    {props.text}
-  </button>
-);
-
-const Display = props => <div> {props.count} </div>;
-
-const Form = props => (
+const Login = props => (
   <form>
-    <input value={props.value} onChange={props.changeHandler} />
+    <input value={props.username} onChange={props.nameChange} />
+    <input value={props.password} onChange={props.passwordChange} />
+
+    {/* Challenge Solution version */}
+    {/* <input value={props.username} onChange={props.bothChange} name='username' />
+    <input value={props.password} onChange={props.bothChange} name='password' />  */}
+
     <button onClick={props.submitHandler}> submit </button>
   </form>
 );
 
-const Message = props => (
+const Inventory = props => (
   <div>
-    <h1>Congrats!</h1>
-    <p>
-      {' '}
-      Your counter is complete! You did it! I'm not totally clear on why I'm
-      congratulating you for clicking a button a bunch of times. Also there's a
-      good chance you skipped the hard work and just put the number straight
-      into the input. But nonetheless, yipee!
-    </p>
+    <h1>Book List</h1>
+    {props.bookList.map(book => (
+      <Book title={book.title} author={book.author} key={book.id} />
+    ))}
   </div>
 );
 
-class App extends React.Component {
-  state = {
-    count: 0,
-    input: ''
-  };
+const Book = props => (
+  <div>
+    <h2>Title: {props.title}</h2>
+    <p>Author: {props.author}</p>
+  </div>
+);
 
-  handleClick = () => {
-    this.setState({ count: this.state.count + 1 });
+class Library extends React.Component {
+  state = {
+    books: [
+      { title: 'Harry Potter', author: 'JK Rowling', id: 0 },
+      { title: 'Universe in a Nutshell', author: 'Stephen Hawking', id: 1 },
+      { title: 'Design Principles', author: 'Gang of Four', id: 2 },
+      { title: 'Lexicon', author: 'Max Barry', id: 3 },
+      { title: 'The Codex', author: 'Lev Grossman', id: 4 },
+      { title: 'Song of Solomon', author: 'Toni Morrison', id: 5 }
+    ],
+    username: '',
+    password: '',
+    isLoggedIn: false
   };
 
   handleSubmit = event => {
     event.preventDefault();
-    this.setState({ count: this.state.input });
+    this.setState({ isLoggedIn: true });
   };
 
-  handleChange = event => {
-    this.setState({ input: event.target.value });
+  usernameChange = event => {
+    this.setState({ username: event.target.value });
   };
+
+  passwordChange = event => {
+    this.setState({ password: event.target.value });
+  };
+
+  // Challenge
+  //   userAndPassChange = (event) => {
+  //     this.setState({ [event.target.name]: event.target.value });
+  //   }
 
   render() {
-    return this.state.count >= 10 ? (
-      <Message />
+    return !this.state.isLoggedIn ? (
+      <Login
+        submitHandler={this.handleSubmit}
+        nameChange={this.usernameChange}
+        username={this.state.username}
+        passwordChange={this.passwordChange}
+        password={this.state.password}
+
+        // bothChange={this.userAndPassChange}
+      />
     ) : (
       <div>
-        <Display count={this.state.count} />
-        <Button
-          buttonStyle="red"
-          text="Click Here"
-          handler={this.handleClick}
-        />
-        <Form
-          submitHandler={this.handleSubmit}
-          changeHandler={this.handleChange}
-          value={this.state.input}
-        />
+        <Inventory bookList={this.state.books} />
       </div>
     );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Library />, document.getElementById('root'));
