2e9a26f056752381480d43b34640610aade720da
Cody Goss <c.ward.goss@gmail.com>
2019-03-07 14:27:56 -0500

    mern fullstack app

--- src/components/SearchPage/NoResult.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const NoResults = () => (
-  <div>
-    <p> No Results... Yet! Search for a category! </p>
-  </div>
-);
-
-export default NoResults;
--- src/components/SearchPage/Result.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Result = props => <p> {props.name} </p>;
-
-export default Result;
